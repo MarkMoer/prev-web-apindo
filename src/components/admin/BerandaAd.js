a1b5556cf05c9715d4b902f4d2de652c76c2417d
farisKamal <faris.kamaaluddin@gmail.com>
2023-01-29 22:45:04 +0700

    minorUpdate

--- src/components/admin/BerandaAd.js
+++ src/components/admin/BerandaAd.js
@@ -4,7 +4,8 @@ import { Container } from 'react-bootstrap'
 const BerandaAd = (props) => {
   return (
     <Container className='mt-5 adminWrapper'>
-        <h1 className='dash-admin'>Selamat Datang {props.name}</h1>
+        <h1 className='dash-admin'>Selamat Datang di</h1>
+        <h1 className='dash-admin'>Dashboard Admin "{props.name}"</h1>
     </Container>
   )
 }
