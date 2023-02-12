7a2147faede1a23fc48212c738cc5495397fe754
farisKamal <faris.kamaaluddin@gmail.com>
2023-02-12 14:28:23 +0700

    testest

--- src/components/admin/AddPictures.js
+++ src/components/admin/AddPictures.js
@@ -55,11 +55,19 @@ const AddPictures = () => {
         formData.append("file", file);
         formData.append("title", title);
         try {
-            await axios.post("https://api-webapindogsk.cyclic.app/pictures", formData, {
+            // await axios.post("https://api-webapindogsk.cyclic.app/pictures", formData, {
+            //     headers: {
+            //       "Content-Type": "application/x-www-form-urlencoded",
+            //     },
+            // });
+            await axios({
+                url: 'https://api-webapindogsk.cyclic.app/pictures',
+                method: 'post',
+                data: formData,
                 headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-            });
+                  'Content-Type': 'multipart/form-data'
+                }
+               })
             navigate("/admin/pictures");
         } catch (error) {
             console.log(error);
