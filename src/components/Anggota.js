dd9caa8a4cfcabbeccf4ab4c9b6f0502b90a3022
farisKamal <faris.kamaaluddin@gmail.com>
2023-12-25 06:42:30 +0700

    fix:bug name

--- src/components/Anggota.js
+++ src/components/Anggota.js
@@ -11,26 +11,26 @@ const Anggota = () => {
       <Row className="justify-content-center fs-5 text-left">
         <Col md>
           <p className="fw-bold mb-0">Dewan Pertimbangan</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Dwi Ken Hendrawanto, SH (PT Ocean Gemindo)</p>
           <p className="fw-bold mb-0">Ketua</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Alfan Wahyuddin, ST,MM (PT Asuka Engineering Indonesia)</p>
           <p className="fw-bold mb-0">Wakil Ketua I</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Herry Wahyono, SE.Ak (PT Kompindo Wiratama)</p>
           <p className="fw-bold mb-0">Wakil Ketua II</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Riduwan, SH (PT Nankai Indonesia)</p>
         </Col>
         <Col md>
           <p className="fw-bold mb-0">Wakil Ketua III</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Muhammad Nur Yusuf (PT Unggul Energi Engineering)</p>
           <p className="fw-bold mb-0">Sekretaris</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Ngadi, SH, MH (PT Multimegah Indahjaya)</p>
           <p className="fw-bold mb-0">Wakil Sekretaris</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>dr. Lukita Hanggraeni (PT Petro Graha Medika)</p>
           <p className="fw-bold mb-0">Bendahara</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lidia Afrilia, SPsi, MM, MH (Pt Keramik Diamond Industries)</p>
         </Col>
       </Row>
-      <div className="d-grid gap-2 formButton ">
+      <div className="d-grid gap-2 formButton pt-2">
         <Button
           variant="outline-dark mt-3 mb-3 text-uppercase"
           size="lg"
@@ -42,4 +42,5 @@ const Anggota = () => {
     </Container>
   );
 };
+
 export default Anggota;
